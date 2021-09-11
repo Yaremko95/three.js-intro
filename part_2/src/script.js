0a5b0e09164f4221d87e90dbde492cc183f7bd0a
tetiana <tetianayaremko@gmail.com>
2021-09-11 15:01:37 +0300

    adds part_2

---
+++ part_2/src/script.js
@@ -0,0 +1,56 @@
+import "./style.css";
+import * as THREE from "three";
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+//group
+const group = new THREE.Group();
+scene.add(group);
+
+group.position.y = 1;
+group.scale.y = 1.2;
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "red" })
+);
+
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "green" })
+);
+cube2.position.x = -2;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "yellow" })
+);
+cube3.position.x = 2;
+group.add(cube3);
+
+//axes helper
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
+
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+
+scene.add(camera);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
